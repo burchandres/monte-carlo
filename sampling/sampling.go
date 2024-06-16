bdf9d84a5ca7d0ad7459db9c34273383534747a1
Andres Burch <acburch22@gmail.com>
2024-06-15 22:08:45 -0400

    linalg package for vector and beginning of sampling package

--- sampling/sampling.go
+++ sampling/sampling.go
@@ -1,2 +1,66 @@
 package sampling
 
+import (
+	"math"
+	"math/rand/v2"
+	"monte-carlo/internal/linalg"
+)
+
+const Pi = math.Pi
+
+// Returns uniformly sampled float in the half open interval [a,b)
+func UniformFromInterval(a, b float64) float64 {
+	// use bijection f(x) = a + x(b-a)
+	u := rand.Float64()
+	dist := math.Abs(b-a)
+	y := a + u*(dist)
+	return y
+}
+
+// Returns point sampled from Normal distribution with
+// mean = m and standard deviation = stdd
+func NormalSample(m, stdd float64) float64 {
+	// Uses Box-Muller transformation
+	u := rand.Float64()
+	v := rand.Float64()
+	sample := stdd*math.Sqrt(u)*math.Cos(v*2*Pi) + m
+	return sample
+}
+
+// Returns uniformly sampled vector along the circumference of the N-Sphere
+// where an N-Sphere is defined by the set
+//						{v \in R^N | |v| = 1}	
+func UniformFromNSphere(N int) linalg.Vector {
+	sample := make([]float64, 0, N)
+	for i := 0; i < N; i++ {
+		u := NormalSample(0,1)
+		sample = append(sample, u)
+	}
+	vector := linalg.Vector{Dim: N, Point: sample}
+	vector, err := linalg.ScalarDiv(vector, vector.Len())
+	if err != nil {
+		// only here if vector was zero vector so return zero vector
+		return linalg.ZeroVector(N)
+	}
+	return vector
+}
+
+// Returns uniformly sampled float from within the N-Ball
+// where an N-Ball is defined by the set
+//						{v \in R^N | |v| < 1}
+func UniformFromNBall(N int) linalg.Vector {
+	sample := UniformFromNSphere(N)
+	d := rand.Float64()
+	return linalg.ScalarMult(sample, math.Pow(d, 1.0/float64(N)))
+}
+
+// Returns point sampled from normal distribution 
+// with mean = m and standard deviation = stdd in R^N
+func NNormalSample(m, stdd float64, N int) linalg.Vector {
+	sample := make([]float64, 0, N)
+	for i := 0; i < N; i++ {
+		sample = append(sample, NormalSample(m, stdd))
+	}
+	sampleVector := linalg.Vector{Dim: N, Point: sample}
+	return sampleVector
+}
